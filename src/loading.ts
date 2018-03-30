6936a633d8956814748ae42f707a7ae4f4659978
Demid Sukhovsky <sukhovsky@hotmail.com>
2018-03-30 14:20:22 +1100

    add a different loading indicator

--- src/loading.ts
+++ src/loading.ts
@@ -1,13 +1,6 @@
 import * as d3 from 'd3';
-import { range } from 'lodash';
 import { Component } from './commonTypes';
 
-interface Arc { endAngle: number; }
-interface Circle { 
-    delay: number;
-    radius: number;
-    color: string;
-}
 export default class LoadingIndicator implements Component {
     selection: d3.Selection<d3.BaseType, {}, null, undefined>;
     width: number;
@@ -33,24 +26,6 @@ export default class LoadingIndicator implements Component {
         }
     }
 
-    spin = (selection: d3.Selection<d3.BaseType, Arc, null, undefined>, duration: number) => {
-        selection.transition()
-            .ease(d3.easeLinear)
-            .duration(duration)
-            .attrTween('transform', function() {
-                return d3.interpolateString('rotate(0)', 'rotate(360)');
-            });
-        setTimeout(() => { this.spin(selection, duration); }, duration);
-    }
-
-    getCircle(i: number): Circle {
-        return {
-            delay: i * 100, 
-            color: d3.schemeCategory10[i % d3.schemeCategory10.length],
-            radius: 46 - i * 6
-        };
-    }
-
     render(): void {
         this.selection.select('svg').remove();        
         const svg = this.selection
@@ -62,34 +37,58 @@ export default class LoadingIndicator implements Component {
         .append('g')
             .attr('transform', `translate(${this.width * 0.5}, ${this.height * 0.5})`);
 
-        svg.selectAll('circle')
-        .data(range(3).map(this.getCircle)).enter()
-        .append('circle')
-            .attr('r', d => d.radius)
-            .attr('fill', 'none')
-            .attr('stroke', d => d.color)
-            .attr('stroke-width', 5)
-        .transition()
-            .duration(250)
-            .ease(d3.easeLinear)
-            .delay(d => d.delay)
-            .on('start', function repeat(c: Circle, index: number, circles: d3.BaseType[]) {
-                const circle = d3.active(circles[index]);
-                if (circle) {
-                    const l = c.radius * Math.PI;
-                    circle
-                        .attr('stroke-dasharray', `0 0 0 ${l} 0 ${l}`)
-                    .transition()
-                        .attr('stroke-dasharray', `0 0 ${l} 0 0 ${l}`)
-                    .transition()
-                        .attr('stroke-dasharray', `0 0 ${l} 0 0 ${l}`)
-                    .transition()
-                        .attr('stroke-dasharray', `0 ${l} 0 ${l} 0 ${l}`)
-                    .transition()
-                        .attr('stroke-dasharray', `0 ${l} 0 ${l} 0 ${l}`)
-                    .transition()
-                        .on('start', repeat);
-                }
-            });
+        const numberOfBars = 10;
+        const barWidth = () => Math.max(1, Math.min(100, Math.floor(Math.min(this.width, this.height) 
+            / numberOfBars / 4)));
+        const size = () => barWidth() * numberOfBars;
+        const barDelay = 100;
+        const boxDelay = 1000;
+        const duration = 1500;
+
+        let count = 0;
+        const translateLeft = (x: number) => `translate(${-this.width / 2 - x - barWidth()})`;
+        const translateRight = (x: number) => `translate(${this.width / 2 - x})`;
+
+        svg.selectAll('rect')
+            .data(d3.range(numberOfBars)).enter()
+            .append('rect')
+            .transition()
+                .on('start', function repeat(d: number, index: number, bars: d3.BaseType[]) {
+                    if (!d) { count++; }
+                    const x = d * barWidth() - size() / 2;
+                    const cx = x + barWidth() / 2;
+                    const color = d3.color(d3.schemeCategory10[(count + 1) % d3.schemeCategory10.length]);
+                    const self = d3.select(bars[index]);
+                    if (self) {
+                        self
+                            .attr('x', x)
+                            .attr('y', - size() / 2)
+                            .attr('transform', translateLeft(x) + ' rotate(0)')
+                            .attr('fill', color.darker().darker().darker().toString())
+                            .attr('width', barWidth())
+                            .attr('height', size())
+                        .transition()
+                            .delay(d * barDelay)
+                            .ease(d3.easeBackOut)
+                            .duration(duration)
+                            .attrTween('transform', () => d3.interpolateString(
+                                translateLeft(x) + `rotate(0, ${cx}, 0)`,
+                                `translate(0) rotate(360, ${cx}, 0)`
+                            ))
+                            .attr('fill', color.toString())
+                        .transition()
+                            .delay(boxDelay + 2 * (numberOfBars - d) * barDelay)
+                            .ease(d3.easeCubicIn)
+                            .duration(duration)
+                            .attrTween('transform', () => d3.interpolateString(
+                                `translate(0) rotate(0, ${cx}, 0)`,
+                                translateRight(d) + ` rotate(360, ${cx}, 0)`
+                            ))
+                            .attr('fill', color.brighter().brighter().brighter().toString())
+                        .transition()
+                            .delay(d * barDelay)
+                            .on('start', repeat);
+                    }
+                });
     }
 }
