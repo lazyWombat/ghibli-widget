e047eff815387406a734bcab2282d161de6c7def
lazyWombat <sukhovsky.waste@gmail.com>
2018-03-30 15:10:45 +1100

    add loading label

--- src/loading.ts
+++ src/loading.ts
@@ -22,7 +22,7 @@ export default class LoadingIndicator implements Component {
                 .attr('width', width)
                 .attr('height', height)
             .select('g')
-                .attr('transform', `translate(${width * 0.5}, ${height * 0.5})`);
+                .attr('transform', `translate(${Math.floor(width * 0.5)}, ${Math.floor(height * 0.5)})`);
         }
     }
 
@@ -35,7 +35,7 @@ export default class LoadingIndicator implements Component {
             .attr('width', this.width)
             .attr('height', this.height)
         .append('g')
-            .attr('transform', `translate(${this.width * 0.5}, ${this.height * 0.5})`);
+            .attr('transform', `translate(${Math.floor(this.width * 0.5)}, ${Math.floor(this.height * 0.5)})`);
 
         const numberOfBars = 10;
         const barWidth = () => Math.max(1, Math.min(100, Math.floor(Math.min(this.width, this.height) 
@@ -48,6 +48,7 @@ export default class LoadingIndicator implements Component {
         let count = 0;
         const translateLeft = (x: number) => `translate(${-this.width / 2 - x - barWidth()})`;
         const translateRight = (x: number) => `translate(${this.width / 2 - x})`;
+        const getColor = () => d3.color(d3.schemeCategory10[(count + 1) % d3.schemeCategory10.length]);
 
         svg.selectAll('rect')
             .data(d3.range(numberOfBars)).enter()
@@ -57,7 +58,7 @@ export default class LoadingIndicator implements Component {
                     if (!d) { count++; }
                     const x = d * barWidth() - size() / 2;
                     const cx = x + barWidth() / 2;
-                    const color = d3.color(d3.schemeCategory10[(count + 1) % d3.schemeCategory10.length]);
+                    const color = getColor();
                     const self = d3.select(bars[index]);
                     if (self) {
                         self
@@ -82,11 +83,30 @@ export default class LoadingIndicator implements Component {
                             .duration(duration)
                             .attrTween('transform', () => d3.interpolateString(
                                 `translate(0) rotate(0, ${cx}, 0)`,
-                                translateRight(d) + ` rotate(360, ${cx}, 0)`
+                                translateRight(x) + ` rotate(360, ${cx}, 0)`
                             ))
                             .attr('fill', color.brighter().brighter().brighter().toString())
                         .transition()
                             .delay(d * barDelay)
+                            .on('start', repeat);
+                    }
+                });
+                
+        svg.append('text')
+            .attr('text-anchor', 'middle')            
+            .text('Loading')
+            .transition()
+                .on('start', function repeat(d: {}, index: number, texts: d3.BaseType[]) {
+                    const self = d3.select(texts[index]);
+                    if (self) {
+                        const fontSize = Math.max(10, size() / 3);
+                        self.transition()
+                            .duration(duration)
+                            .attr('y', size() / 2 + fontSize)
+                            .attr('font-size', `${fontSize}px`)
+                            .attr('fill', getColor().darker().toString())
+                        .transition()
+                            .delay(boxDelay + duration + 2 * numberOfBars * barDelay)
                             .on('start', repeat);
                     }
                 });
